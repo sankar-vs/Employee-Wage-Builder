c753d056f13f20adfd0dc77e0fd66e88053c9b4c
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-08 16:31:45 +0530

    [Sankar]ADDED:Calculation of Employee Wage for a Part Time Employee

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,14 +2,19 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		 //Constraints
                 int IS_FULL_TIME = 1;
-                int empRatePerHr = 20;
+                int IS_PART_TIME = 2;
+		int empRatePerHr = 20;
                 int empHrs;
                 float wage;
-                double empCheck = Math.floor(Math.random() * 10) % 2;
+                double empCheck = Math.floor(Math.random() * 10) % 3;
                 //Computation
                 if (empCheck == IS_FULL_TIME) {
-                        System.out.println("Employee is Present");
+                        System.out.println("Employee is Present_Full_Time");
                         empHrs = 8;
+                }
+		else if (empCheck == IS_PART_TIME) {
+                        System.out.println("Employee is Present_Part_Time");
+                        empHrs = 4;
                 }
                 else {
                         System.out.println("Employee is Absent");
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 #### Check if the Employee is Present or Absent (Using Random function for attendance check)
 ### UsedCase:2
 #### Calculate Employee Wage assuming Wage per Day is 20 and Full Day Working Hour is 8.
+### UsedCase:3
+#### Calculate Employee Wage for a Part Time Employee assuming Full Day Working Hour is 4.
