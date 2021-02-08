2a78b9c8f0096b2a5a69620300802a732427d764
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-08 16:20:34 +0530

    [Sankar]ADDED:Calculate Wage for Employee Present

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,12 +1,21 @@
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
-		//Constraints
+		 //Constraints
                 int IS_FULL_TIME = 1;
+                int empRatePerHr = 20;
+                int empHrs;
+                float wage;
                 double empCheck = Math.floor(Math.random() * 10) % 2;
                 //Computation
-                if (empCheck == IS_FULL_TIME)
+                if (empCheck == IS_FULL_TIME) {
                         System.out.println("Employee is Present");
-                else
+                        empHrs = 8;
+                }
+                else {
                         System.out.println("Employee is Absent");
+                        empHrs = 0;
+                }
+                wage = empRatePerHr*empHrs;
+                System.out.println("Salary: "+wage);
 	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Welcome to Employee Wage Builder Program
 ### UsedCase:1 
 #### Check if the Employee is Present or Absent (Using Random function for attendance check)
+### UsedCase:2
+#### Calculate Employee Wage assuming Wage per Day is 20 and Full Day Working Hour is 8.
