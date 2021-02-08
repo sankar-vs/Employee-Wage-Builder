ec0346832ada2208c873cca23973ee1edff9fc85
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-08 16:08:38 +0530

    [Sankar]ADDED:Code to check if Employee Present or Absent

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,12 @@
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		//Constraints
+                int IS_FULL_TIME = 1;
+                double empCheck = Math.floor(Math.random() * 10) % 2;
+                //Computation
+                if (empCheck == IS_FULL_TIME)
+                        System.out.println("Employee is Present");
+                else
+                        System.out.println("Employee is Absent");
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Welcome to Employee Wage Builder Program
+### UsedCase:1 
+#### Check if the Employee is Present or Absent (Using Random function for attendance check)
