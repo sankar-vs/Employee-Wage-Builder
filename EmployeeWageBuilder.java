eebcfa0ce8952adf847dccf9cd00c41c5f75efb6
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-10 18:30:46 +0530

    [Sankar]REFACTORED:Created a class method

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,29 +5,36 @@ public class EmployeeWageBuilder {
         public static final int MAX_HRS = 100;
         public static final int WORKING_DAYS = 20;
 
+	public static int workHrs(int check) {
+		int workingHours;
+		switch(check) {
+                	case IS_FULL_TIME:
+                        	System.out.println("Employee is Present_Full_Time");
+                                workingHours = 8;
+                                break;
+                        case IS_PART_TIME:
+                                System.out.println("Employee is Present_Part_Time");
+                                workingHours = 4;
+                                break;
+                        default:
+                                System.out.println("Employee is Absent");
+                                workingHours = 0;
+                        }
+		return workingHours;
+
+	}
 	public static void main(String[] args) {
 		//Constraints
                 int empHrs;
 		int Hrs = 0;
 		int day = 1;
                 float wagePerDay = 0, totalSalary = 0;
-                //Computation
+
+		//Computation
 		while ( (day <= WORKING_DAYS) && (Hrs < MAX_HRS) ) {
-                        double empCheck = Math.floor(Math.random() * 10) % 3;
-	                switch((int)empCheck) {
-		                case IS_FULL_TIME:
-	        	                System.out.println("Employee is Present_Full_Time");
-                        	        empHrs = 8;
-	                                break;
-        	                case IS_PART_TIME:
-                	                System.out.println("Employee is Present_Part_Time");
-                        	        empHrs = 4;
-                                	break;
-                       		default:
-                                	System.out.println("Employee is Absent");
-                             		empHrs = 0;
-			}
-			day += 1;
+                        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+	                empHrs = workHrs(empCheck);
+	                day += 1;
                         Hrs += empHrs;
                 	wagePerDay = EMP_RATE_PER_HR * empHrs;
 			totalSalary += wagePerDay;
