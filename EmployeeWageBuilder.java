af1104d550d75b9ed23d8280acfb022bc9e9fd4e
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-10 17:08:28 +0530

    [Sankar]REFACTORED:Constant variable correction

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,8 +1,8 @@
 public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
-        public static final int empRatePerHr = 20;
-        public static final int workingDays = 20;
+        public static final int EMP_RATE_PER_HR = 20;
+        public static final int WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 		//Constraints
@@ -10,7 +10,7 @@ public class EmployeeWageBuilder {
 		int day = 1;
                 float wagePerDay = 0, totalSalary = 0;
                 //Computation
-		for ( day = 1 ; day <= workingDays ; day++ ) {
+		for ( day = 1 ; day <= WORKING_DAYS ; day++ ) {
                         double empCheck = Math.floor(Math.random() * 10) % 3;
 	                switch((int)empCheck) {
 		                case IS_FULL_TIME:
@@ -25,7 +25,7 @@ public class EmployeeWageBuilder {
                                 	System.out.println("Employee is Absent");
                              		empHrs = 0;
 			}
-                	wagePerDay = empRatePerHr * empHrs;
+                	wagePerDay = EMP_RATE_PER_HR * empHrs;
 			totalSalary += wagePerDay;
 		}
                 System.out.println("Salary: "+ totalSalary);
